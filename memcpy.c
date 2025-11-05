5f0bed39ce70395d44fbf86c8dfd25cc913018a1
smenard <floogles71@gmail.com>
2025-11-05 14:11:15 +0100

    feat: memcpy

--- memcpy.c
+++ memcpy.c
@@ -1,35 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   memcpy.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smenard <smenard@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/03 15:07:56 by smenard           #+#    #+#             */
-/*   Updated: 2025/11/05 11:49:54 by smenard          ###   ########lyon.fr   */
+/*   Created: 2025/11/05 11:48:31 by smenard           #+#    #+#             */
+/*   Updated: 2025/11/05 11:54:56 by smenard          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <unistd.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
 
-int		ft_isdigit(int c);
-
-int		ft_isalnum(int c);
-
-int		ft_isascii(int c);
-
-int		ft_isprint(int c);
-
-size_t	ft_strlen(const char *str);
-
-void	*ft_memset(void *s, int c, size_t n);
-
-void	ft_bzero(void *s, size_t n);
-
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-
-#endif
+	i = 0;
+	while (i < n)
+	{
+		(*((unsigned char *)src + i)) = (*((unsigned char *)dest + i));
+		i++;
+	}
+	return (dest);
+}
