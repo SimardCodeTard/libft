0bd2f2365dc4a5c39d82397a0b00052942155301
smenard <floogles71@gmail.com>
2025-11-06 12:35:47 +0100

    chore: renamed files to ft_[name]

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,29 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strlcpy.c                                          :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smenard <smenard@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/05 14:38:58 by smenard           #+#    #+#             */
-/*   Updated: 2025/11/05 16:38:32 by smenard          ###   ########lyon.fr   */
+/*   Created: 2025/11/05 11:48:31 by smenard           #+#    #+#             */
+/*   Updated: 2025/11/06 10:43:09 by smenard          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, char *src, size_t size)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (!dest && !src)
+		return (NULL);
 	i = 0;
-	while (src[i] && i < size)
+	while (i < n)
 	{
-		dst[i] = src[i];
+		(*((unsigned char *)dest + i)) = (*((unsigned char *)src + i));
 		i++;
 	}
-	dst[size - 1] = 0;
-	while (src[i])
-		i++;
-	return (i);
+	return (dest);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -1,26 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memcpy.c                                           :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smenard <smenard@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/05 11:48:31 by smenard           #+#    #+#             */
-/*   Updated: 2025/11/05 14:30:47 by smenard          ###   ########lyon.fr   */
+/*   Created: 2025/11/05 14:14:19 by smenard           #+#    #+#             */
+/*   Updated: 2025/11/06 10:43:26 by smenard          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	int	i;
 
-	i = 0;
-	while (i < n)
+	if (!dest && !src)
+		return (NULL);
+	if (((long) src) < ((long) dest))
 	{
-		(*((unsigned char *)dest + i)) = (*((unsigned char *)src + i));
-		i++;
+		i = n - 1;
+		while (i >= 0)
+		{
+			((unsigned char *) dest)[i] = ((unsigned char *) src)[i];
+			i--;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < (int) n)
+		{
+			((unsigned char *) dest)[i] = ((unsigned char *) src)[i];
+			i++;
+		}
 	}
 	return (dest);
 }
