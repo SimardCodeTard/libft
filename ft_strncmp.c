a3b8623f99f37b5cc46cfdbe3fa518a4164b2664
Gerard2par2 <floogles71@gmail.com>
2025-11-08 18:27:05 +0100

    fix(strncmp): remove unused variable found

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,12 +14,10 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int					found;
 	size_t				i;
 	const unsigned char	*u_s1 = (unsigned char *) s1;
 	const unsigned char	*u_s2 = (unsigned char *) s2;
 
-	found = 0;
 	i = 0;
 	while ((u_s1[i] || u_s2[i]) && i < n)
 	{
