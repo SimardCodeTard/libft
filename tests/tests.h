60960c09a6a2a67e5cc638b93606c0352fbd9c8a
Gerard2par2 <floogles71@gmail.com>
2025-11-09 16:02:49 +0100

    fix(tests): typo in tests.h

--- tests/tests.h
+++ tests/tests.h
@@ -6,13 +6,13 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 15:30:38 by smenard           #+#    #+#             */
-/*   Updated: 2025/11/09 15:37:48 by smenard          ###   ########.fr       */
+/*   Updated: 2025/11/09 15:54:39 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TESTS_H
 # define TESTS_H
-# include "macros/colors.h"K<
+# include "macros/colors.h"
 # include "macros/test_helpers.h"
 # include "struct/test_case_string.h"
 # include <unistd.h>
