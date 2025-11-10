8d0f2bb8684481b17fe27413379571319035bd3c
smenard <floogles71@gmail.com>
2025-11-10 11:02:17 +0100

    fix(calloc): return NULL when nmemb or size is NULL

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smenard <smenard@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 13:32:49 by smenard           #+#    #+#             */
-/*   Updated: 2025/11/08 20:45:37 by smenard          ###   ########.fr       */
+/*   Updated: 2025/11/10 10:18:49 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	if (nmemb * size > INT_MAX)
 		return (NULL);
 	if (nmemb == 0 || size == 0)
-		return (malloc(1));
+		return (NULL);
 	s = malloc(nmemb * size);
 	if (!s)
 		return (NULL);
