1752883236577589449187aed1d60e39170f69e6
Gerard2par2 <floogles71@gmail.com>
2025-11-11 16:45:06 +0100

    refactor(tests_strim): use new test runner

--- tests/ft_strtrim/struct_test_strtrim.h
+++ tests/ft_strtrim/struct_test_strtrim.h
@@ -6,20 +6,25 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 15:09:26 by smenard           #+#    #+#             */
-/*   Updated: 2025/11/09 17:59:30 by smenard          ###   ########.fr       */
+/*   Updated: 2025/11/11 16:44:22 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_TEST_STRTRIM_H
 # define STRUCT_TEST_STRTRIM_H
-# include "../tests.h"
+
+typedef struct s_params_strtrim
+{
+	char	*desc;
+	char	*s1;
+	char	*set;
+	char	*expected;
+}	t_params_strtrim;
 
 typedef struct s_test_case_strtrim
 {
-	const char	*desc;
-	const char	*s1;
-	const char	*set;
-	const char	*expected;
+	t_test_result		(*f)(void *);
+	t_params_strtrim	params;
 }	t_test_case_strtrim;
 
 #endif
