2f066c46ca31e6bc0ffe7eeb09432ef4c23369af
smenard <floogles71@gmail.com>
2025-11-12 14:28:12 +0100

    feat(lstclear): handle cyclic lists

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -1,21 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 13:56:26 by smenard           #+#    #+#             */
-/*   Updated: 2025/11/12 13:59:12 by smenard          ###   ########.fr       */
+/*   Updated: 2025/11/12 14:26:43 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static bool	lst_has_cycle(t_list *lst)
+{
+	const t_list	*the_chosen_one = lst;
+
+	if (!lst)
+		return (false);
+	while (lst->next)
+	{
+		lst = lst->next;
+		if (lst == the_chosen_one)
+			return (true);
+	}
+	return (false);
+}
+
+static void	ft_lstclear_cyclic(t_list **lst, void (*del)(void *),
+				t_list *the_chosen_one)
+{
+	if (*lst == the_chosen_one)
+		return ;
+	ft_lstclear_cyclic(&(*lst)->next, del, the_chosen_one);
+	del((*lst)->content);
+	free(*lst);
+	*lst = NULL;
+}
+
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	if (!lst || !*lst)
 		return ;
+	if (lst_has_cycle(*lst))
+		return (ft_lstclear_cyclic(lst, del, *lst));
 	ft_lstclear(&(*lst)->next, del);
 	del((*lst)->content);
 	free(*lst);
